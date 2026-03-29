881faca2d2f673d2f732ac51f788c85d3feca5e7
dogu <alagozdogu@gmail.com>
2026-03-29 22:17:05 +0300

    commit increase

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // Crashlytics global hata yakalama!!
+  // Crashlytics global hata yakalama!!!!
   FlutterError.onError = (errorDetails) {
     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
   };
